4100353cac39d687a29ba106dc28df405d85ccd6
oo w <refout@outlook.com>
2023-08-13 00:03:13 +0800

    docs(trace): 添加注释

--- trace-datasource/src/main/java/com/refout/trace/datasource/convert/enums/StateEnumConvert.java
+++ trace-datasource/src/main/java/com/refout/trace/datasource/convert/enums/StateEnumConvert.java
@@ -3,9 +3,21 @@ package com.refout.trace.datasource.convert.enums;
 import com.refout.trace.common.enums.StateEnum;
 import jakarta.persistence.Converter;
 
+/**
+ * 状态枚举转换器类，用于将状态枚举类型转换为数据库列的值。
+ *
+ * @author oo w
+ * @version 1.0
+ * @since 2023/8/12 23:42
+ */
 @Converter(autoApply = true)
 public class StateEnumConvert extends AbstractEnumConvert<StateEnum> {
 
+    /**
+     * 获取状态枚举类型的数组。
+     *
+     * @return 状态枚举类型的数组
+     */
     @Override
     protected StateEnum[] getEnums() {
         return StateEnum.values();
--- trace-redis/src/main/java/com/refout/trace/redis/config/RedisConfig.java
+++ trace-redis/src/main/java/com/refout/trace/redis/config/RedisConfig.java
@@ -17,29 +17,64 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
 
+/**
+ * Redis配置类
+ *
+ * @author oo w
+ * @version 1.0
+ * @since 2023/8/12 23:42
+ */
 @EnableCaching
 @Configuration
 public class RedisConfig {
 
+    /**
+     * 缓存前缀
+     */
     private static final String PREFIX = "SPRING";
 
+    /**
+     * 创建RedisTemplate实例
+     *
+     * @param connectionFactory Redis连接工厂
+     * @param <K>               键类型
+     * @param <V>               值类型
+     * @return RedisTemplate实例
+     */
     @Bean
     public <K, V> RedisTemplate<K, V> redisTemplate(RedisConnectionFactory connectionFactory) {
+        // 创建Json序列化器
         var jsonRedisSerializer = genericJackson2JsonRedisSerializer();
+        // 创建String序列化器
         var stringRedisSerializer = new StringRedisSerializer();
 
+        // 创建RedisTemplate实例
         RedisTemplate<K, V> redisTemplate = new RedisTemplate<>();
+        // 设置连接工厂
         redisTemplate.setConnectionFactory(connectionFactory);
+        // 设置键的序列化器
         redisTemplate.setKeySerializer(stringRedisSerializer);
+        // 设置值的序列化器
         redisTemplate.setValueSerializer(jsonRedisSerializer);
+        // 设置Hash键的序列化器
         redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        // 设置Hash值的序列化器
         redisTemplate.setHashValueSerializer(jsonRedisSerializer);
+        // 初始化RedisTemplate
         redisTemplate.afterPropertiesSet();
+        // 返回RedisTemplate实例
         return redisTemplate;
     }
 
+    /**
+     * 创建RedisCacheManager实例
+     *
+     * @param connectionFactory Redis连接工厂
+     * @return RedisCacheManager实例
+     */
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        // 创建Redis缓存配置
         RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
                 .serializeKeysWith(RedisSerializationContext.string().getKeySerializationPair())
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
@@ -49,6 +84,7 @@ public class RedisConfig {
                 .computePrefixWith((name) -> CacheKeyRule.keyEndWithSeparator(PREFIX, name))
                 .entryTtl(Duration.ofHours(1));
 
+        // 构建RedisCacheManager实例
         return RedisCacheManager
                 .builder(connectionFactory)
                 .cacheDefaults(configuration)
@@ -56,8 +92,14 @@ public class RedisConfig {
                 .build();
     }
 
+    /**
+     * 创建GenericJackson2JsonRedisSerializer实例
+     *
+     * @return GenericJackson2JsonRedisSerializer实例
+     */
     @Contract(" -> new")
     private @NotNull GenericJackson2JsonRedisSerializer genericJackson2JsonRedisSerializer() {
+        // 使用JsonUtil创建GenericJackson2JsonRedisSerializer实例
         return new GenericJackson2JsonRedisSerializer(JsonUtil.mapperWithType());
     }
 
