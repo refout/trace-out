e64ada8deb7a3a8c3a1e00074c25daf021d52ec0
oo w <refout@outlook.com>
2023-08-16 08:44:53 +0800

    refactor(分页查询): 更新分页查询接口示例

--- trace-system/src/main/java/com/refout/trace/system/controller/UserController.java
+++ trace-system/src/main/java/com/refout/trace/system/controller/UserController.java
@@ -3,9 +3,10 @@ package com.refout.trace.system.controller;
 import com.refout.trace.common.system.domain.User;
 import com.refout.trace.common.system.service.UserService;
 import com.refout.trace.common.web.domain.Result;
-import com.refout.trace.datasource.domain.page.PageIn;
+import com.refout.trace.datasource.domain.query.QueryRequest;
+import com.refout.trace.datasource.domain.query.page.PageIn;
+import com.refout.trace.datasource.domain.query.page.PageOut;
 import jakarta.annotation.Resource;
-import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +20,8 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/page")
-    public Result page(@RequestBody PageIn<User> pageIn) {
-        Page<User> page = userService.getPage(pageIn);
+    public Result page(@RequestBody PageIn<User, QueryRequest<User>> pageIn) {
+        PageOut<User> page = userService.getPage(pageIn);
         return Result.success(page);
     }
 
